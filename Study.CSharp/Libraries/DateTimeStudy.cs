17275596029135bdd346bc83cb6c0b71d4341b06
dgw <shijiucha@qq.com>
2014-07-16 14:49:00 +0800

    dgw：DateTime.ParseExact study.

---
+++ Study.CSharp/Libraries/DateTimeStudy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+using NUnit.Framework;
+
+namespace Study.CSharp.Libraries
+{
+    [TestFixture]
+    public sealed class DateTimeStudy
+    {
+        [Test]
+        public void ParseExact_Study()
+        {
+            var result = DateTime.ParseExact("2012年01月01日", "yyyy年MM月dd日", null, DateTimeStyles.None);
+
+            Assert.AreEqual(result, new DateTime(2012, 01, 01));
+        }
+    }
+}
